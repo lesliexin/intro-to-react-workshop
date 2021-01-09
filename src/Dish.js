c7279a82d436aaf371dc5f1d3eb3654be30dfaf9
lesliexin <leslie.xin@hotmail.com>
2021-01-08 20:15:54 -0500

    Create React App 4: Adding interactivity

--- src/Dish.js
+++ src/Dish.js
@@ -6,14 +6,21 @@ export class Dish extends React.Component {
         this.state = {
             counter: 0
         };
-    };
+    }
+
+    addDish = () => {
+        this.setState({
+            counter: this.state.counter+1
+        });
+    }
 
     render() {
         return (
             <div className="Dish">
                 <h2>{this.props.name}</h2>
                 <p>Amount: {this.state.counter}</p>
+                <button onClick={this.addDish}>Add dish</button>
             </div>
         );
-    };
+    }
 }
